42268df0f376bbd9f533c099f9e5039d2e3df0f0
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2023-12-18 17:07:03 +0100

    feat(:tada:): Create project

---
+++ composeApp/src/jsMain/kotlin/com/ailtontech/animeseas/App.js.kt
@@ -0,0 +1,7 @@
+package com.ailtontech.animeseas
+
+import kotlinx.browser.window
+
+internal actual fun openUrl(url: String?) {
+    url?.let { window.open(it) }
+}
---
+++ composeApp/src/jsMain/kotlin/com/ailtontech/animeseas/theme/Theme.js.kt
@@ -0,0 +1,7 @@
+package com.ailtontech.animeseas.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun SystemAppearance(isDark: Boolean) {
+}
